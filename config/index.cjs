944a05259ad30a62cb01e119d027c29a749b8aaf
TX1020 <99231947+TX1020@users.noreply.github.com>
2022-09-25 23:47:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wx0124847844ac7f34',
+  APP_ID: '',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: 'b82389367d4829e1a44ffeebb97116d2',
+  APP_SECRET: '',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
